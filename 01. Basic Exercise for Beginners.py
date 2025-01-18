8dae51d275ecea67dc9c2298bf1f45aaf238bc42
sbo <sbuik@trapo.de>
2025-01-18 15:06:22 +0100

    First task

---
+++ 01. Basic Exercise for Beginners.py
@@ -0,0 +1,23 @@
+"""
+Task: Given two integer numbers, write a Python code to return their product only if the product is equal to\
+      or lower than 1000. Otherwise, return their sum.
+
+Given: number1 = 20, number2 = 30
+Expected output: The result is 600
+"""
+
+def calculate(a,b):
+    if(a*b<=1000):
+        return a*b
+    else:
+        return a+b
+
+#Example 1
+a = 20
+b = 30
+print("1The result is "+ str(calculate(a,b)))
+
+#Example 2
+a = 40
+b = 30
+print("2The result is "+ str(calculate(a,b)))
